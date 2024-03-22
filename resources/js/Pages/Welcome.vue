52a6d4e8616987dd40c8ef5a2469930d0cda2798
suk-jedsada <162295583+suk-jedsada@users.noreply.github.com>
2024-03-22 14:22:07 +0700

    feat: change color and background

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="p-10 bg-red-500 w-screen h-screen">
+    <div class="w-screen h-screen p-10 bg-yellow-600">
         <button
-            class="w-32 md:w-40 lg:w-60 xl:w-96 px-4 py-2 rounded-sm bg-pink-800 text-white transition transform duration-200 ease-in-out hover:scale-105">
-            sdhfgjdhgs
+            class="w-32 px-4 py-2 text-white transition duration-200 ease-in-out transform bg-pink-800 rounded-sm md:w-40 lg:w-60 xl:w-96 hover:scale-105">
+            เชื่อมต่อสำเร็จ
         </button>
     </div>
 </template>
